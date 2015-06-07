1eb0e8b56c1726f182475325832fc1bb34cf3272
Roma <ichenkoroma@gmail.com>
2015-06-07 22:18:34 +0600

    add font awesome to layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
-
+  config.assets.precompile += %w(
+    *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif
+  )
 end

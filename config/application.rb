f215017742e0538759bcb010a2df40404ffb28d9
Roma <ichenkoroma@gmail.com>
2015-06-09 20:35:12 +0600

    add user attributes locale

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module EJournal
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.i18n.default_locale = :ru
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
